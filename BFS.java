f212a5e65e534a5ff25286807136d015d44f7497
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2026-07-29 22:47:55 +0530

    BFS

--- BFS.java
+++ BFS.java
@@ -1,3 +1,27 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
 public class BFS {
-    
+    public list<String> BFS(int start, Map<Integer, List<Integer>> adj, int n){
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        boolean[] visited = new boolean[n+1];
+
+        queue.offer(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            result.add(node);
+            for(int neigh :adj.getOrDefault(node, new ArrayList<>())){
+                if(!visited[neigh]){
+                    visited[neigh] = true;
+                    queue.offer(neigh);
+                }
+            }
+        }
+        return result;
+    }
 }
