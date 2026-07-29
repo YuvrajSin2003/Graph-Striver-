9927e0f0eb51a9964fd053a24c2dc18a71183e4a
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2026-07-29 22:58:40 +0530

    DFS

---
+++ DFS.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.Map;
+
+public class DFS {
+    public void DFS(int node, Map<Integer, List<Integer>> adj, boolean[] visited, List<Integer> result){
+        visited[node] = true;
+        result.add(node);
+        for(int neigh :adj.getOrDefault(node, new ArrayList<>())){
+            if(!visited[neigh]){
+                DFS(neigh , adj visited , result);
+            }
+        }
+        return result;
+    }
+}
